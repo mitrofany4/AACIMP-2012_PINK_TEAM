fa923ffe19fc8f5356ec210a68e37f3d49c94be5
madatx <azick9@gmail.com>
2012-08-06 17:15:47 +0300

    Basic Hero Movements

--- AngryPeople/HeroMovements.js
+++ AngryPeople/HeroMovements.js
@@ -6,36 +6,52 @@
  * To change this template use File | Settings | File Templates.
  */
 // a hero structure
-var hero = {
+var heroModel = {
     position:    120,
     state:       "up",
     weaponInUse: "paperBalls"
 };
 // a man on the balcony
-var currentHero = new hero;
+//var heroModel = hero;
 var heroObject;
+var balcony;
+
+// initialize the balcony and hero html-objects
+function initObjects(){
+    balcony = document.getElementById("balcony");
+    heroObject = document.getElementById("hero");
+}
 
 // movment to the left
 function leftAction(){
-    heroObject = document.getElementById("hero");
-    // hero position without 'px'
-    var position = parseInt(heroObject.style.left);
-    position -= 10;
-    // js-object
-    heroObject.position -= 10;
-    // html-object
-    heroObject.style.left = position + 'px';
+    initObjects();
+    // checking if the object is out of balcony
+    var absoluteLeftBarrier = balcony.offsetLeft;
+    if (heroModel.position >= absoluteLeftBarrier){
+         // hero position without 'px'
+        var position = parseInt(heroObject.style.left);
+        position -= 10;
+        // js-object
+        heroModel.position -= 10;
+        // html-object
+        heroObject.style.left = position + 'px';
+    }
 }
 
 // movement to the right
 function rightAction(){
-    heroObject = document.getElementById("hero");
-    // hero position without 'px'
-    var position = parseInt(heroObject.style.left);
-    position += 10;
-    // js-object
-    heroObject.position += 10;
-    // html-object
-    heroObject.style.left = position + 'px';
-
+    initObjects();
+    // checking if the object is out of balcony
+    var absoluteRightBarrier = balcony.offsetWidth + balcony.offsetLeft - heroObject.offsetWidth;
+    if (heroModel.position <= absoluteRightBarrier){
+        // hero position without 'px'
+        var position = parseInt(heroObject.style.left);
+        position += 10;
+        // js-object
+        heroModel.position += 10;
+        // html-object
+        heroObject.style.left = position + 'px';
+    }
 }
+
+
