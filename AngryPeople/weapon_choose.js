edcdf19d2417f53bd2ceb9bb6e9af5fa741b0b50
Pasha <tipasha@list.ru>
2012-08-08 17:03:42 +0300

    pasha

--- AngryPeople/weapon_choose.js
+++ AngryPeople/weapon_choose.js
@@ -1,15 +1,10 @@
-var selected;
-
+var drop;
 function setSpit (){
-    selected = 0;
     heroModel.weaponInUse = 0;
     spit = document.getElementById("spit");
-    return selected;
 }
 
 function setBomb (){
-    selected = 1;
     heroModel.weaponInUse = 1;
     spit = document.getElementById("waterbomb");
-    return selected;
 }
