11a553b357ef15476052f88043ee0081c8d6b2fe
mitrofany4 <mitrofany4@GMAIL.COM>
2012-08-10 12:37:33 +0300

    fixed people appearance and rewrote people`s classes

--- AngryPeople/walking_man.js
+++ AngryPeople/walking_man.js
@@ -8,26 +8,90 @@
 // create a Person
 
 var ordinary_url = "url('walking_man.png')";
+var retired_url = "url('retired.png')";
 
-function newPerson(pos,dir){
-    return{
-    speed  : 5,
-//    xacc   : 0,
-    ypos   : 20,
-    xpos   : pos,
-    dir    : dir
-        };
+ var retired = {
+        xpos    :   0,
+        ypos    :   20,
+        speed   :   5,
+        dir     :   "left",
+        percent :   0,
+        koef    :   0.5
 }
 
-function set_dir(____person){
-    if (____person.dir === "left"){
-        ____person.speed *= (-1);
+var ordinary = {
+    xpos    :   0,
+    ypos    :   20,
+    speed   :   3,
+    dir     :   "left",
+    percent :   0,
+    koef    :   1
+}
+/*
+function Person(dir,pos){
+
+
+    this.update=function(){
+        if ((this.xpos < Math.abs(this.speed) + 15) || (this.xpos > Math.abs(this.speed) + 15))
+            this.xpos += this.speed;
+
     }
 }
-function updatePerson(___person){
+*/
+function Ordinary(pos,dir){
+
+    //variables
+
+    this.xpos=pos;
+    this.ypos=20;
+    this.speed=5;
+    this.dir=dir;
+    this.percent=0;
+    this.koef=1;
+
+    //functions
+    if (this.dir === "left"){
+        this.speed *= (-1);
+    }
+
+    this.update=function(){
+        if ((this.xpos < Math.abs(this.speed) + 15) || (this.xpos > Math.abs(this.speed)))
+            this.xpos += this.speed;
+
+    }
+}
+
+function Retired(pos,dir){
+
+    //variables
+
+    this.xpos=pos;
+    this.ypos=20;
+    this.speed=3;
+    this.dir=dir;
+    this.percent=0;
+    this.koef=0.5
+
+    //functions
+
+    if (this.dir === "left"){
+        this.speed *= (-1);
+    }
+
+    this.update=function(){
+        if ((this.xpos < Math.abs(this.speed) + 15) || (this.xpos > Math.abs(this.speed) + 15))
+            this.xpos += this.speed;
+
+    }
+
+}
+
+
+/*function updatePerson(___person){
 
     // Navigation
     if ((___person.xpos < Math.abs(___person.speed) + 15) || (___person.xpos > Math.abs(___person.speed) + 15))
         ___person.xpos += ___person.speed;
 
 }
+*/
