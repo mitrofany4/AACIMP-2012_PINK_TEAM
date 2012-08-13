ba511ec74ae1c163e1e7e16251d79a1d45e5d00a
Dimon <DimitrYo>
2012-08-13 11:49:09 +0300

    some changes in timer and etc

--- AngryPeople/infopanel.js
+++ AngryPeople/infopanel.js
@@ -1,9 +1,8 @@
-var seconds = 60;
-
 function levelchange (level) {
     var levelnum = document.getElementById('levelnumber');
     levelnum.children[0].innerHTML=level;
 }
+levelchange(1);
 
 
 function progress (first, second ) {
@@ -11,49 +10,36 @@ function progress (first, second ) {
                 pr.children[0].innerHTML=first;
                 pr.children[1].innerHTML=second;
 }
+progress(0,10);
 
-/*
-function leveltime1 () {
-                seconds-=1;
-                var minutes = Math.floor(seconds / 60);
-                var timer = document.getElementById('time');
-                timer.children[0].innerHTML=minutes;
-                if (seconds < 10)
-                timer.children[1].innerHTML='0'+seconds;
-                else
-                timer.children[1].innerHTML=seconds;
-                if (seconds > 0) {
-//                <!--Time Over functions-->
-//                alert('TIME OVER');
-//                    clearInterval(intervid1);
-//                    $.mobile.changePage( "index.html", { transition: "slideup"} );
-                }
-    setTimeout(leveltime1,1000);
-}
-*/
-var timer;
 
-var time;
-function myTimer(){
-    var minutes = Math.floor(seconds / 60);
-/*    time = document.getElementById('time');
-    time.children[0].innerHTML=minutes;
+var seconds = 60;
+var minutes = 0;
+var times = seconds;
+var timer = document.getElementById('time');
+
+function levelstime() {
+//    alert("sdfds");
+    $("#time").everyTime(1000, 'timer2' ,function() {
+    seconds--;
+    minutes = Math.floor(seconds / 60);
+    timer.children[0].innerHTML=minutes;
     if (seconds < 10)
-        time.children[1].innerHTML='0'+seconds;
+    {
+        timer.children[1].innerHTML='0'+seconds;
+    }
     else
-        time.children[1].innerHTML=seconds;*/
-    clearInterval(timer);
-    timer = setInterval(function() {
-        $('#time').text(seconds--);
-        if (seconds == -1) {
-            clearInterval(timer);
-            alert('done');
-        }
-    }   , 1000);
+    {
+        timer.children[1].innerHTML=seconds;
+    }
+//    if (seconds == 0) {
+//        timer.stopTime('timer2');
+//        alert('done');
+//    }
 }
+    , times);
+}
+levelstime();
 
-
-
-//var intervid1  = setInterval(leveltime1,1000);
 
 
