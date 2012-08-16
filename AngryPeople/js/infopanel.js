dd9fc95d286895591108994254b0126befad5f9a
Dimon <DimitrYo>
2012-08-16 13:04:03 +0300

    Level end

--- AngryPeople/js/infopanel.js
+++ AngryPeople/js/infopanel.js
@@ -10,8 +10,6 @@ function progress (first, second ) {
                 pr.children[0].innerHTML=first;
                 pr.children[1].innerHTML=second;
 }
-//timer
-//progress(1,10);
 
 var timer;
 var seconds = 120;
@@ -43,10 +41,10 @@ function myTimer(){
         if (seconds == 0) {
             clearInterval(timer);
 
-            if ($("#progress").children[0] >= 7)
+            if (parseInt($("#progress").children[0]) >= 1)
                 timeisend('You are the winner!');
             else
-                timeisend("Try Again");
+                timeisend('Try Again');
 
             for (var i=0; i<num; i++){
                 DivHuman[i].style.display="none";
@@ -60,17 +58,19 @@ function timeisend(textinbox) {
     $("#dialogendoftime").css("top", parseInt(gamearea.offsetHeight - dialogendoftime.offsetHeight)/2 + 'px');
     $("#dialogendoftime").css("left",parseInt(gamearea.offsetWidth - dialogendoftime.offsetWidth)/2 + 'px');
     $("#dialogendoftime").css("display","block");
-    if (textinbox = "Try Again")
+    $("#textdialog").text(textinbox);
+    if (textinbox == 'Try Again')
     {
-        $("#tryagain").style.display = "block";
-        $("#youwin").style.display = "none";
+        $("#tryagain").css("display", "block");
+        $("#youwin").css("display","none");
     } else
     {
-        $("#tryagain").style.display = "none";
-        $("#youwin").style.display = "block";
+        $("#tryagain").css("display", "none");
+        $("#youwin").css("display","block");
+        level+=1;
+        localStorage.setItem('levelsolve',level);
     }
-    $("#textdialog").text(textinbox);
 }
-
+progress(7,10);
 
 
